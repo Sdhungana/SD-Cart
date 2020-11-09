bae1d03a7fc96bc205caae93998a77def41f7f6c
Sulab Dhungana <sulabknight@gmail.com>
2020-11-09 18:14:05 +0545

    Create items.js

---
+++ items.js
@@ -0,0 +1,23 @@
+//----------  Item List-------------//
+const items = [
+    { itemName: 'Leather Bag', itemImage: '1.jpg', itemPrice: '$93.78' },
+    { itemName: 'Air pod', itemImage: '2.jpg', itemPrice: '$150.78' },
+    { itemName: 'Louis Vutton Shoe', itemImage: '3.jpg', itemPrice: '$230.78' },
+    { itemName: 'Guitar', itemImage: '4.jpg', itemPrice: '$53.78' },
+    { itemName: 'Laptop Bag', itemImage: '5.jpg', itemPrice: '$23.78' },
+    { itemName: 'JBL Headphone', itemImage: '6.jpg', itemPrice: '$540.78' },
+    { itemName: 'Leather Bag', itemImage: '1.jpg', itemPrice: '$93.78' },
+    { itemName: 'Air pod', itemImage: '2.jpg', itemPrice: '$150.78' },
+    { itemName: 'Louis Vutton Shoe', itemImage: '3.jpg', itemPrice: '$230.78' },
+    { itemName: 'Guitar', itemImage: '4.jpg', itemPrice: '$53.78' },
+    { itemName: 'Laptop Bag', itemImage: '5.jpg', itemPrice: '$23.78' },
+    { itemName: 'JBL Headphone', itemImage: '6.jpg', itemPrice: '$540.78' },
+    { itemName: 'Leather Bag', itemImage: '1.jpg', itemPrice: '$93.78' },
+    { itemName: 'Air pod', itemImage: '2.jpg', itemPrice: '$150.78' },
+    { itemName: 'Louis Vutton Shoe', itemImage: '3.jpg', itemPrice: '$230.78' },
+    { itemName: 'Guitar', itemImage: '4.jpg', itemPrice: '$53.78' },
+    { itemName: 'Laptop Bag', itemImage: '5.jpg', itemPrice: '$23.78' },
+    { itemName: 'JBL Headphone', itemImage: '6.jpg', itemPrice: '$540.78' },
+];
+
+export { items }
